c7256bc39d9856307cc5be1aebbc9df8a635fca3
kamilb13 <kamil.brzoza13@gmail.com>
2024-11-24 21:34:24 +0100

    added lessons

--- src/screens/LessonsScreen/index.js
+++ src/screens/LessonsScreen/index.js
@@ -1,10 +1,44 @@
-import {Button, Text, View} from 'react-native';
+import {Button, Text, TouchableOpacity, View} from 'react-native';
 
 const LessonsScreen = ({ navigation }) => {
 
+    const lessons = [
+        {
+            id: "1",
+            name: "Greetings"
+        },
+        {
+            id: "2",
+            name: "Numbers"
+        },
+        {
+            id: "3",
+            name: "Colors"
+        },
+        {
+            id: "4",
+            name: "Basic Phrases"
+        }
+    ]
+
+    const handlePress = (lessonName) => {
+        navigation.navigate("LessonDetail")
+        console.log(`${lessonName.name} clicked`);
+    };
+
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Lekcje</Text>
+            {lessons.map((item, index) =>
+                <TouchableOpacity
+                    key={index}
+                    style={{padding: 10, margin: 5, backgroundColor: "#fff", borderRadius: 10}}
+                    onPress={() => handlePress(item)}
+                >
+                    <Text>
+                        {item.name}
+                    </Text>
+                </TouchableOpacity>
+            )}
         </View>
     );
 }
