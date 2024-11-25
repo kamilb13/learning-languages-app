6ed67ad2240f33368f3bb766bd0b9b0c25fb297a
kamilb13 <kamil.brzoza13@gmail.com>
2024-11-25 18:26:57 +0100

    simple lesson detail screen

--- src/screens/LessonDetailScreen/index.js
+++ src/screens/LessonDetailScreen/index.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const LessonDetailScreen = ({ route }) => {
-    const lessonId = route.params;
+    console.log(route)
+    const lessonId = route.params.lesson.id;
+    const tasks = route.params.lesson.tasks;
+    const name = route.params.lesson.name;
 
     return (
-        <View>
-            <Text>Numer lekcji {lessonId}</Text>
+        <View style={styles.container}>
+            <Text style={styles.text}>Numer lekcji: {lessonId}</Text>
+            <Text style={styles.text}>Nazwa {name}</Text>
+            {/*<Text style={styles.text}>Zadania: {tasks}</Text>*/}
+            {tasks.map((task) =>
+                <Text style={{fontSize: 18}}>{task}</Text>
+            )}
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    text: {
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+});
 
 export default LessonDetailScreen;
